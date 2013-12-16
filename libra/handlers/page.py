c117a8de6765bc0d15869b00167f2eb7479687c6
Victor Pantoja <victor.pantoja@gmail.com>
2013-12-16 10:15:27 -0200

    Adding loggin

--- libra/handlers/page.py
+++ libra/handlers/page.py
@@ -1,5 +1,27 @@
 # coding: utf-8
+from html.parser import HTMLParser
 from tornado.web import RequestHandler
+
+from urllib import request
+
+inTD = False
+
+
+class MyHTMLParser(HTMLParser):
+    def handle_starttag(self, tag, attrs):
+        global inTD
+        if tag.upper() == "META":
+            inTD = True
+
+    def handle_endtag(self, tag):
+        global inTD
+        if tag.upper() == "META":
+            inTD = False
+
+    def handle_data(self, data):
+        global inTD
+        if inTD:
+            print(data)
 
 
 class PageHandler(RequestHandler):
@@ -8,6 +30,9 @@ class PageHandler(RequestHandler):
         self.render("index.html")
 
     def post(self, **kwargs):
-        url = self.get_argument("url")
+        parser = MyHTMLParser()
+        page = request.urlopen(self.get_argument("url")).read().decode("utf-8")
+        parser.feed(page)
 
+        import pdb;pdb.set_trace()
         self.render("index.html")
