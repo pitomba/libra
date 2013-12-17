cc89c1bc0f8b38532ae5ff35010912cd7783f0af
Darlene <darlene.santos@corp.globo.com>
2013-12-16 22:19:16 -0200

    Modify index css.

--- libra/static/js/user.js
+++ libra/static/js/user.js
@@ -1,26 +1,26 @@
 var user = {
-	init: function(){
-		$('create_page').addEvent('submit', function(e){
-		    e.stop();
-		    user.savePages()
-		});
-	},
+    init: function(){
+        $('create_page').addEvent('submit', function(e){
+            e.stop();
+            user.savePages();
+        });
+    },
 		
 	savePages: function(){
-		  var request = new Request({
-			  async: false,
-			  method: 'post',
-			  noCache: false,
-			  onSuccess: function(responseText, responseXML){
-				  console.log(responseText, responseXML)
-			  },
-			  onFailure: function(xhr){
-				  console.log(xhr);
-			  },
-			  url: '/api/page/'
-		  });
+        var request = new Request({
+            async: false,
+            method: 'post',
+            noCache: false,
+            onSuccess: function(responseText, responseXML){
+                console.log(responseText, responseXML);
+            },
+            onFailure: function(xhr){
+                console.log(xhr);
+            },
+            url: '/api/page/'
+        });
 
-		  request.post($('create_page').toQueryString());
+        request.post($('create_page').toQueryString());
 	}
 };
 
