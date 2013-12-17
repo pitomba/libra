b41355f67a05f5a273bc2d1e68ef041ae94e6987
Romulo Jales <romulo.jales@corp.globo.com>
2013-12-16 22:31:59 -0200

    nem lembro mais

--- libra/processor/page_analytic.py
+++ libra/processor/page_analytic.py
@@ -4,11 +4,12 @@ from processor.processor import TagProcessor
 
 
 class PageAnalytic(object):
-
+    headers={"User-Agent": "Mozilla/5.0 (X11; U; Linux i686) Gecko/20071127 Firefox/2.0.0.11",
+             "Accept-Encoding": "gzip"}
     tagsClass = TagProcessor.__subclasses__()
 
     def __init__(self, page):
-        response = requests.get(page)
+        response = requests.get(page, headers=self.headers)
         self.html_size = int(response.headers["content-length"])
         self.page_object = BeautifulSoup(response.content)
         self._tags = []
@@ -16,7 +17,7 @@ class PageAnalytic(object):
     def _get_tag_size(self, tag):
         for subCls in self.tagsClass:
             if subCls.is_resource(tag):
-                self._tags.append(subCls(tag))
+                self._tags.append(subCls(tag, base_url=))
                 return True
         else:
             return False
@@ -26,7 +27,7 @@ class PageAnalytic(object):
         self.page_object.find_all(self._get_tag_size)
         for tag in self._tags:
             #threading?
-            resp = requests.head(tag.get_resource_url())
+            resp = requests.head(tag.get_resource_url(), headers=self.headers)
             if resp.status_code == 200:
                 size += int(resp.headers["content-length"])
         self._tags = []
